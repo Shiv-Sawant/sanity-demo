5010e28c9a5e939090d476cd54a6915f81401e0f
Shiv <shivsawant1958@gmail.com>
2023-02-15 18:58:44 +0530

    added

---
+++ sanityyoutube/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'w73liu43',
+    dataset: 'production'
+  }
+})
---
+++ sanityyoutube/schemas/index.js
@@ -0,0 +1,8 @@
+import blockContent from './blockContent'
+import category from './category'
+import posts from './post' 
+import author from './author'
+import comment from './comment'
+
+
+export const schemaTypes = [posts, author, category, blockContent, comment, ]
